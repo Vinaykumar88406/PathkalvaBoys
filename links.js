8812595c897307d0e56d32709e3c1a52d14ef3c2
Vinaykumar88406 <146372455+Vinaykumar88406@users.noreply.github.com>
2025-02-23 18:02:58 +0530

    Update links.js

--- links.js
+++ links.js
@@ -7,6 +7,7 @@ function myFriend(){
     bgcolor.style.backgroundColor=color;
 
     document.getElementById("message").innerHTML="MODDA GUDUV RA,...PUUUUKAA...surprise kavalanta ediki..😂😂😂";
+       document.getElementById("button").innerText="double click chey";
 }
 function myFriend2(){
     let color="green"
