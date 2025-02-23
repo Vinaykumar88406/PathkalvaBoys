c4a9ca6d14f221ff458860f70a3ae733171d8081
Vinaykumar88406 <146372455+Vinaykumar88406@users.noreply.github.com>
2025-02-23 17:51:38 +0530

    Add files via upload

---
+++ links.js
@@ -0,0 +1,18 @@
+       
+
+
+function myFriend(){
+    let color="yellow"
+    let bgcolor= document.getElementById("message");
+    bgcolor.style.backgroundColor=color;
+
+    document.getElementById("message").innerHTML="MODDA GUDUV RA,...PUUUUKAA...surprise kavalanta ediki..😂😂😂";
+}
+function myFriend2(){
+    let color="green"
+    let bgcolor= document.getElementById("message");
+    bgcolor.style.backgroundColor=color;
+
+    document.getElementById("message").innerHTML="Vellu ra Rey enni sarlu avthavu..ERRipuka...😂😂😂";
+}
+
